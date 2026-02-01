c4f03a25afe7c17073b5e02ce3cdc6a3219e56df
yarintaberi <yarintaberi@gmail.com>
2026-02-01 21:02:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const http = require("http");
 const { addonBuilder } = require("stremio-addon-sdk");
 
 const manifest = {
@@ -16,13 +17,20 @@ builder.defineStreamHandler(({ id }) => {
   if (id !== "cbs") return Promise.resolve({ streams: [] });
 
   return Promise.resolve({
-    streams: [{
-      title: "CBS Live",
-      url: "https://list.iptvcat.com/my_list/s/1d0316d039eedda854185dd09a33a72a.m3u8"
-    }]
+    streams: [
+      {
+        title: "CBS Live",
+        url: "https://list.iptvcat.com/my_list/s/1d0316d039eedda854185dd09a33a72a.m3u8"
+      }
+    ]
   });
 });
 
-module.exports = (req, res) => {
+// ⭐ חובה ב-BeamUp
+const port = process.env.PORT || 7000;
+
+http.createServer((req, res) => {
   builder.getInterface()(req, res);
-};
+}).listen(port, () => {
+  console.log("Stremio addon running on port " + port);
+});
